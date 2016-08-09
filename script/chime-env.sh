74beeac9c0d6d54544f2379016005b13a1b924e8
xiaoyang <muma.378@163.com>
2016-08-09 10:28:39 +0800

    [ADD] service to start|stop mesos and hdfs

---
+++ script/chime-env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+masters="hou-Lenovo-Product"
+slaves="hou-Lenovo-Product;shujutang-ThinkCentre-E73"
+
+MESOS_DAEMON="/usr/local/sbin/mesos-daemon.sh"
+#USER_HOME='/usr/local'
+USER_HOME=$sw
